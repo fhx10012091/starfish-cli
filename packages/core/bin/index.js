f02f3976ec5033cb7bf7dbf38a1e4d14a99e1f16
fhx10012091 <1875934025@qq.com>
2021-06-27 17:43:18 +0800

    one week commit

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
+const util = require('@starfish-cli/utils')
+util()
 console.log('hello starfish-cli 努力啊！！！！')
--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -4,4 +4,5 @@ module.exports = utils;
 
 function utils() {
     // TODO
+    console.log('您好啊！666')
 }
